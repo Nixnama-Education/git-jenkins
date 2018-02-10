4592d9bd68e1f92879a9825a53108f71507055be
Nixnama-Education <34838071+Nixnama-Education@users.noreply.github.com>
2018-02-10 08:50:55 +0000

    Update publishoverssh.sh

--- publishoverssh.sh
+++ publishoverssh.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 echo creating directory
+mkdir /common/ssh-test/buildtest
 touch /common/ssh-test/buildtest/publishoverssh.txt
 echo "successfully tested" >> /common/ssh-test/buildtest/publishoverssh.txt
